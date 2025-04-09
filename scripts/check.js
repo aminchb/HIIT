f129398400e2a58aee4577cb8d46306980c1033f
unknown <amin.choubai@gmail.com>
2025-04-09 05:47:21 +0200

    V1.2 : check

--- scripts/check.js
+++ scripts/check.js
@@ -1,20 +1,25 @@
-// DEPLOYMENTS :
+// CHECK :
+const website_url = "https://aminchb.github.io/hiit/";
 const deployments_url = "https://github.com/aminchb/hiit/deployments";
 
-function deployments(){
+function check(){
     if (typeof window !== "undefined" && typeof window.open === "function") {
+        window.open(website_url, "_blank");
         window.open(deployments_url, "_blank");
     } else {
         const { exec } = require("child_process");
         const platform = process.platform;
         if (platform === "win32") {
+            exec(`start ${website_url}`);
             exec(`start ${deployments_url}`);
         } else if (platform === "darwin") {
+            exec(`open ${website_url}`);
             exec(`open ${deployments_url}`);
         } else {
+            exec(`xdg-open ${website_url}`);
             exec(`xdg-open ${deployments_url}`);
         }
     }
 }
 
-deployments();
+check();
